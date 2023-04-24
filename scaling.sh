3ce338b68bb8e3a96c8abcca8582874f24a5fe67
Nutulapati Sriaditya <nsriaditya@IN-NSRIADIT-MBP.local>
2023-04-24 16:28:41 +0530

    fm1

--- scaling.sh
+++ scaling.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "hello"
-apt-get update  # To get the latest package lists
-apt-get install atlas -y
+yum update  # To get the latest package lists
+yum install atlas -y
 atlas cluster update $1 --projectId 644298165b26bb5f7b771ce5 --tier $2
