4f02b6c26fefb0073b60f117553d79eb5341f846
Nutulapati Sriaditya <nsriaditya@IN-NSRIADIT-MBP.local>
2023-04-24 18:27:57 +0530

    fm1

--- scaling.sh
+++ scaling.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 xcode-select --install
+curl -fsSL -o install.sh https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+/bin/bash install.sh
+nano ~/.zshrc
+nano ~/.bash_profile
 echo "hello"
+export PATH=/usr/local/bin:$PATH
+source ~/.zshrc
+source ~/.bash_profile
+brew doctor
  # To get the latest package lists
 # atlas cluster update $1 --projectId 644298165b26bb5f7b771ce5 --tier $2
