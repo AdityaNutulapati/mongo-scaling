a52fa334ec6e756a4036ac575ff04792014bf889
Nutulapati Sriaditya <nsriaditya@IN-NSRIADIT-MBP.local>
2023-04-24 17:33:03 +0530

    fm1

--- scaling.sh
+++ scaling.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "hello"
  # To get the latest package lists
-atlas cluster update $1 --projectId 644298165b26bb5f7b771ce5 --tier $2
+# atlas cluster update $1 --projectId 644298165b26bb5f7b771ce5 --tier $2
