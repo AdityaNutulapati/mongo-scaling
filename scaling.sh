b4d3c080d7de540c2c4ffbac3721dc80008215bb
Nutulapati Sriaditya <nsriaditya@IN-NSRIADIT-MBP.local>
2023-04-24 18:24:03 +0530

    fm1

--- scaling.sh
+++ scaling.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-xcode-select -install
+xcode-select --install
 echo "hello"
  # To get the latest package lists
 # atlas cluster update $1 --projectId 644298165b26bb5f7b771ce5 --tier $2
