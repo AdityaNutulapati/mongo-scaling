f18eacd1630350ba1ae602cfd7cb9dfecc690f00
Nutulapati Sriaditya <nsriaditya@IN-NSRIADIT-MBP.local>
2023-04-25 09:58:49 +0530

    ff

--- scaling.sh
+++ scaling.sh
@@ -5,12 +5,12 @@
 # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # /bin/bash install.sh
 # nano ~/.bash_profile
-# echo "hello"
+echo "hello"
 # export PATH=/usr/local/bin:$PATH
 # source ~/.bash_profile
 # brew doctor
 # wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_1.0.1ubuntu2.17_amd64.deb -O apt.deb
 # which wget
-apt-get install --only-upgrade mongodb-atlas-cli
+# apt-get install --only-upgrade mongodb-atlas-cli
  # To get the latest package lists
 # atlas cluster update $1 --projectId 644298165b26bb5f7b771ce5 --tier $2
