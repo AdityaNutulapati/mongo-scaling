0e6286c2d670aec810698eee327754a2e697c1d7
Nutulapati Sriaditya <nsriaditya@IN-NSRIADIT-MBP.local>
2023-04-25 09:53:28 +0530

    fere

--- scaling.sh
+++ scaling.sh
@@ -9,7 +9,8 @@
 # export PATH=/usr/local/bin:$PATH
 # source ~/.bash_profile
 # brew doctor
-wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_1.0.1ubuntu2.17_amd64.deb -O apt.deb
+# wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_1.0.1ubuntu2.17_amd64.deb -O apt.deb
+# which wget
 apt-get install --only-upgrade mongodb-atlas-cli
  # To get the latest package lists
 # atlas cluster update $1 --projectId 644298165b26bb5f7b771ce5 --tier $2
