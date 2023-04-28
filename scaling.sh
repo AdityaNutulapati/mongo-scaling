ae9067e5af9f877b157622bfb25bfd3a64284ce0
Nutulapati Sriaditya <AdityaNutulapati>
2023-04-28 10:14:29 +0530

    adfb

--- scaling.sh
+++ scaling.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-atlas config set public_api_key $1 | atlas config set private_api_key $2 | atlas cluster update $3  --projectId $4  --tier $5
+/opt/homebrew/bin/atlas config set public_api_key $1 | /opt/homebrew/bin/atlas config set private_api_key $2 | /opt/homebrew/bin/atlas cluster update $3  --projectId $4  --tier $5
